56747fc6d9a9d7a354dde80ca8f0c62349a3c671
Shrijan Aryal <shrijanaryal@MacBook-Pro.local>
2017-06-01 11:36:45 -0700

    Modified the array to ask users for input

--- lab1.cpp
+++ lab1.cpp
@@ -13,23 +13,30 @@ void bubblesort(string array[],int len){
             }
         }
     }
-
+    
 }
 
 int main()
 {
     cout << "Hello World! This is Shrijan and this is my first lab" << endl;
     cout << "This is lab1.cc"<< endl;
-    string array[] = {"Shrijan","Pranaya","Ashok","Aashish"};
-    int len = sizeof(array)/sizeof(array[0]);
-    //Print the contents of the unsorted array
+    int len = 0;
+    cout << "How many names would you like to sort ?: ";
+    cin >> len;
+    string array[len] ;
+    string name;
+    for (int i = 0; i < len; i++){
+        cout << "Enter the name: ";
+        cin >> name;
+        array[i] = name;
+    }
     cout << "\n**** Printing unsorted array ****"<<endl;
     for (int i = 0; i<len; i++){
         cout << array[i]<<" ";
     }
     bubblesort(array,len);
     //Print the contents of sorted array
-    cout << "\n\n**** Printing unsorted array ****"<<endl;
+    cout << "\n\n**** Printing sorted array ****"<<endl;
     for (int i = 0; i<len; i++){
         cout << array[i]<<" ";
     }
@@ -40,10 +47,15 @@ int main()
 /*
  Hello World! This is Shrijan and this is my first lab
  This is lab1.cc
+ How many names would you like to sort ?: 4
+ Enter the name: Strawberry
+ Enter the name: Banana
+ Enter the name: Apple
+ Enter the name: Google
  
  **** Printing unsorted array ****
- Shrijan Pranaya Ashok Aashish
+ Strawberry Banana Apple Google
  
- **** Printing unsorted array ****
- Aashish Ashok Pranaya Shrijan Program ended with exit code: 0
+ **** Printing sorted array ****
+ Apple Banana Google Strawberry
  */
